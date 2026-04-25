7815958015a2f39f5dca6fbc77df2414038e08e9
Kaoru Yoshida <kaoru.yoshida@keio.jp>
2026-04-26 05:37:51 +0900

    memcmp int->char

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -15,11 +15,11 @@
 
 int	memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned int p1;
-	unsigned int p2;
+	unsigned char p1;
+	unsigned char p2;
 	
-	p1 = (const unsigned int)s1;
-	p2 = (const unsigned int)s2;
+	p1 = (const unsigned char)s1;
+	p2 = (const unsigned char)s2;
 	if (n == 0)
 		return (0);
 	while (n > 0)
@@ -31,4 +31,26 @@ int	memcmp(const void *s1, const void *s2, size_t n)
 		n--;
 	}
 	return (0);
+}
+
+int main(void)
+{
+	char str1[] = "Hello, World!";
+	char str2[] = "Hello, World!";
+	size_t n = 5;
+
+	int result = memcmp(str1, str2, n);
+	if (result == 0)
+	{
+		printf("The first %zu bytes of str1 and str2 are equal.\n", n);
+	}
+	else if (result < 0)
+	{
+		printf("The first %zu bytes of str1 are less than those of str2.\n", n);
+	}
+	else
+	{
+		printf("The first %zu bytes of str1 are greater than those of str2.\n", n);
+	}
+	return 0;
 }
